81d0d8af6162ff259c39f7f72b8809241e871f26
JohnCrossRollOn <juanrolon54@outlook.com>
2023-01-31 22:39:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+
+function App() {
+
+  return (
+    <div className="min-h-screen ">
+    </div>
+  )
+}
+
+export default App
